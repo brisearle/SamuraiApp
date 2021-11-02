b2cbebdce2a19094b7026d97d5986f361d38478b
Brian Searle <bsearle@silvergate.com>
2021-11-02 14:46:13 -0700

    First Module
    Basic Entity Core setup. First module completed

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using SamuraiApp.Data;
+using SamuraiApp.Domain;
+using System;
+using System.Linq;
 
 namespace SamuraiApp.UI
 {
     class Program
     {
+        private static SamuraiContext _context = new SamuraiContext();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _context.Database.EnsureCreated();
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            Console.WriteLine("Press any Key...");
+            Console.ReadKey();
+        }
+
+        private static void AddSamurai()
+        {
+            var samurai = new Samurai { Name = "Sampson" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+        
+        private static void GetSamurais(string text)
+        {
+            var samuraies = _context.Samurais.ToList();
+            Console.WriteLine($"{text}: Samurai count is {samuraies.Count}");
+            foreach (var samurai in samuraies)
+            {
+                Console.WriteLine(samurai.Name);
+            }
         }
     }
 }
